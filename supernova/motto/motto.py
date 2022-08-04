80ccec80bfbbc1345ac43ab7ee31ba85a1f7fd44
HuangJiameng <105633685+HuangJiameng@users.noreply.github.com>
2022-08-04 14:38:19 +0800

    Devel (#3)
    * Update README.md
    
    * Update README.md
    
    * Update motto.py
    
    * Update motto.py
    
    * Update motto.py
    
    * Update motto.py
    
    * update
    
    * update
    
    * update
    
    * update
    
    * Create __init__.py

--- supernova/motto/motto.py
+++ supernova/motto/motto.py
@@ -1,5 +1,6 @@
 import argparse
 from supernova.utils import md5_encoding
+from supernova.toybox.JiamengHuang import verifyMottoJiamengHuang
 
 def verifyMotto(P: argparse.Namespace):
     name = P.name
@@ -13,7 +14,7 @@ def verifyMotto(P: argparse.Namespace):
         assert RuntimeError("Your input name is not valid for this test!")
 
     if is_verified:
-        print(f"Your input '{motto}' mˇatches with {name}'s motto! ")
+        print(f"Your input '{motto}' matches with {name}'s motto! ")
     else:
         print(f"Your input '{motto}' doesn't match with {name}'s motto! ")
 
@@ -35,26 +36,5 @@ def verifyMottoDPTechnology(public_string: str) -> bool:
     md5_string = md5_encoding(public_string)
     is_verified = False
     if md5_string == "894f4cc4639856a9345e8c686818528f":
-        is_verified = True
-    return is_verified
-
-
-def verifyMottoJiamengHuang(public_string: str) -> bool:
-    '''
-
-    Verify if a public string is JiamengHuang's motto.
-
-    Parameters:
-    ---------
-    public_string: string, a public string from user input.
-
-    Returns:
-    -------
-    is_verified: bool, if the string matches with the certain motto,
-        return True.
-    '''
-    md5_string = md5_encoding(public_string)
-    is_verified = False
-    if md5_string == "2418e06198cb88fca52e2d20658e16fa":
         is_verified = True
     return is_verified
